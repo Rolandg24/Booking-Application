417778e7ac47a9a21abd82d2c100a61ddc5db62f
Roland Garza <garza.roland24@gmail.com>
2024-05-20 22:44:22 -0400

    Pushing User inputs

--- main.go
+++ main.go
@@ -4,12 +4,21 @@ import "fmt"
 
 func main() {
 
-	var conferenceName = "Go Conference"
-	const conferenceTickets = 1250
-	var remainingTickets = 1250
+	conferenceName := "8-Bit Gaming Conference"
+	const conferenceTickets int = 1250
+	var remainingTickets int = 1250
 
-	fmt.Println("Welcome to the official", conferenceName, "Booking Application")
-	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
-	fmt.Println("Get your tickets today to attend!")
+	fmt.Printf("conferenceTickets is %T, remainingTickets is %T, conferenceName is %T\n", conferenceTickets, remainingTickets, conferenceName)
+
+	fmt.Printf("Welcome to the official %v Booking Application\n", conferenceName)
+	fmt.Printf("We have a total of %v tickets and %v are still available.\n", conferenceTickets, remainingTickets)
+	fmt.Printf("Get your tickets today to attend!\n")
+
+	var userName string
+	var userTickets int
+	//asking user for their name here.
+	fmt.Scan(userName)
+	userTickets = 4
+	fmt.Printf("%v booked %v tickets. \n", userName, userTickets)
 
 }

dc6587881cdb9422b37800ef2451a90e362ab639
Roland Garza <garza.roland24@gmail.com>
2024-05-20 21:01:39 -0400

    Pushing Variable Names

--- main.go
+++ main.go
@@ -3,5 +3,13 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Print("Hello World")
+
+	var conferenceName = "Go Conference"
+	const conferenceTickets = 1250
+	var remainingTickets = 1250
+
+	fmt.Println("Welcome to the official", conferenceName, "Booking Application")
+	fmt.Println("We have a total of", conferenceTickets, "tickets and", remainingTickets, "are still available.")
+	fmt.Println("Get your tickets today to attend!")
+
 }
